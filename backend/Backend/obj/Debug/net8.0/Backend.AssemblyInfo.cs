59e86ed6c853b8b800ede07f33b6326893aad1b5
ansinha27 <ansinha27@gmail.com>
2025-11-02 20:11:39 +0000

    fixed marker issue

--- backend/Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ backend/Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28fee84a8bd8903068db5b7dc6d4185bf26553ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+934f780cef3165720cb7e25e0d2790f2a16f24b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
